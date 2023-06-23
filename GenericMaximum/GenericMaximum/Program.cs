f27dae04c3db4891cea9298af9e6388ca346c22b
Prachi <prachikale136@gmail.com>
2023-06-23 20:53:05 +0530

    [Prachi] Add: find the maximum integer  number

---
+++ GenericMaximum/GenericMaximum/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMaximum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+           // MaximumGenerics maximumGenerics = new MaximumGenerics();
+           int check1= MaximumGenerics.MaximumNumber(50,30,10);
+            Console.WriteLine(check1);
+        }
+    }
+}
